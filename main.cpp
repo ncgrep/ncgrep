1d47e056e78e8a05f228621398775bec65798f03
huxu <huxu@didichuxing.com>
2017-12-01 17:35:28 +0800

    :art: improve the display

--- main.cpp
+++ main.cpp
@@ -34,9 +34,10 @@ int main(int argc, char ** argv)
     initscr();
     cbreak();
     noecho();
+    keypad(stdscr, true);
     curs_set(0); // hiden the cursor
 
-    int yMax, xMax, yWin, xWin;
+    unsigned long yMax, xMax, yWin, xWin;
     int curLine = 1;
     yWin = 20;
     xWin = 100;
@@ -46,23 +47,28 @@ int main(int argc, char ** argv)
     box(win, 0, 0);
 
     // print line to win
-    //wattron(win, A_REVERSE);
-    for (int i = 0; i < yWin - 2; ++i) {
+    for (unsigned long i = 0; i < yWin - 2; ++i) {
         int line = i + 1;
         if (curLine == line) {
             wattron(win, A_REVERSE);
         }
-        string text(mfv[i].filename + " " + to_string(mfv[i].line) + " " + mfv[i].content.substr(0, 60));
-        string space((xWin - text.length() - 2), ' ');
+        string text(mfv[i].filename + " " + to_string(mfv[i].line) + " " + mfv[i].content.substr(0, xWin));
+        string space;
+        if (text.length() > xWin - 2) {
+            text = text.substr(0, xWin - 2);
+        } else if(text.length() < xWin - 2) {
+            space = string(xWin - text.length() - 2, ' ');
+        }
         mvwprintw(win, line, 1, (text + space).c_str());
         if (curLine == line) {
             wattroff(win, A_REVERSE);
         }
     }
-    //wattroff(win, A_REVERSE);
     wrefresh(win);
 
-    int c = getch();
+    int c;
+    while ((c = getch())) {
+    }
 
     endwin();
 }
