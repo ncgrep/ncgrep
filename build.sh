f22b58ab3485f1e271700c83cec9101b880bdaff
GenialX <admin@ihuxu.com>
2017-12-02 16:46:25 +0800

    :sparkles: scroll win

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 g++ -c files.cpp -o files.o
 g++ -c grep.cpp -o grep.o
-g++ -g main.cpp -o grepx files.o grep.o -lncurses
+g++ -c tui.cpp -o tui.o
+g++ -c data.cpp -o data.o
+g++ -g main.cpp -o grepx files.o grep.o tui.o data.o -lncurses
 ./grepx s .
--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 
 #include "files.h"
 #include "grep.h"
+#include "tui.h"
+#include "data.h"
 
 using namespace std;
-
-void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_files> mfv, unsigned long cur_line);
 
 int main(int argc, char ** argv)
 {
@@ -18,26 +18,13 @@ int main(int argc, char ** argv)
     }
 
     // Get data
-    vector<string> files = listdir(argv[2], 0);
-    vector<match_files> mfv, tmp;
-    for (unsigned long i = 0; i < files.size(); ++i) {
-        //cout<<files[i]<<endl;
-        try {
-            tmp = match_pattern(files[i], argv[1]);
-            mfv.insert(mfv.end(), tmp.begin(), tmp.end());
-        } catch (runtime_error &e) {
-            cerr<<e.what()<<endl;
-            return 1;
-        }
+    vector<match_files> mfv;
+    try {
+        mfv = get_data(argv[2], argv[1]);
+    } catch (runtime_error &e) {
+        cerr<<e.what()<<endl;
+        return 1;
     }
-    /*
-    for (unsigned long j = 0; j < mfv.size(); ++j) {
-        cout<<"filename:"<<mfv[j].filename<<endl;
-        cout<<"line:"<<mfv[j].line<<endl;
-        cout<<"content:"<<mfv[j].content<<endl<<endl;
-    }
-    return 0;
-    */
 
     // Ncurses initialization
     setlocale(LC_ALL,"");
@@ -66,47 +53,21 @@ int main(int argc, char ** argv)
             case 'q':
                 do_continue = false;
                 break;
-        }
-        switch (c) {
-            case KEY_UP:
-                --cur_line;
-                refresh_win(win, yWin, xWin, mfv, cur_line);
+            case 'k':
+                if (cur_line == 0) {
+                    break;
+                }
+                refresh_win(win, yWin, xWin, mfv, --cur_line);
                 break;
-            case KEY_DOWN:
-                ++cur_line;
-                refresh_win(win, yWin, xWin, mfv, cur_line);
-                break;
-            case KEY_LEFT:
-                break;
-            case KEY_RIGHT:
+            case 'j':
+                if (cur_line == mfv.size() - 1) {
+                    break;
+                }
+                refresh_win(win, yWin, xWin, mfv, ++cur_line);
                 break;
         }
     }
 
     endwin();
     return 0;
-}
-
-void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_files> mfv, unsigned long cur_line) {
-    unsigned long max_line = (yWin - 2) < mfv.size() ? yWin - 2 : mfv.size();
-    unsigned long start_line = cur_line < (max_line / 2) ? 0 : cur_line - max_line / 2;
-    for (unsigned long i = start_line; i < max_line + start_line; ++i) {
-        if (cur_line == i) {
-            wattron(win, A_REVERSE);
-        }
-        // @TODO repalce the tab to 4 space chars
-        string text(mfv[i].filename + "+" + to_string(mfv[i].line) + " " + mfv[i].content.substr(0, xWin));
-        string space;
-        if (text.length() > xWin - 2) {
-            text = text.substr(0, xWin - 2);
-        } else if(text.length() < xWin - 2) {
-            space = string(xWin - text.length() - 2, ' ');
-        }
-        mvwprintw(win, i - start_line + 1, 1, (text + space).c_str());
-        if (cur_line == i) {
-            wattroff(win, A_REVERSE);
-        }
-    }
-    wrefresh(win);
-    return;
 }
