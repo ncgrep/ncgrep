5b46da60f2c46c56b16c731a506e4bd3f3a4df9d
GenialX <admin@ihuxu.com>
2017-12-02 23:48:03 +0800

    :sparkles: enter to open file by vim

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 #include "files.h"
 #include "grep.h"
@@ -9,6 +10,11 @@
 #include "data.h"
 
 using namespace std;
+
+void init_screen();
+unsigned long yMax, xMax, yWin, xWin;
+unsigned long cur_line = 0;
+WINDOW * win;
 
 int main(int argc, char ** argv)
 {
@@ -26,29 +32,26 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    // Ncurses initialization
-    setlocale(LC_ALL,"");
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, true);
-    curs_set(0); // hiden the cursor
+    // Init screen
+    init_screen();
 
     // Print window
-    unsigned long yMax, xMax, yWin, xWin;
-    unsigned long cur_line = 0;
-    getmaxyx(stdscr, yMax, xMax);
-    yWin = long(yMax * 0.6);
-    xWin = long(xMax * 0.8);
-    WINDOW * win = newwin(yWin, xWin, (yMax - yWin) /2, (xMax - xWin) / 2);
-    refresh();
-    box(win, 0, 0);
     refresh_win(win, yWin, xWin, mfv, cur_line);
 
     // Keyboard input
     int c;
     bool do_continue = true;
     while (do_continue && (c = getch())) {
+        switch (c) {
+            case 10:
+                string cmd = "vim " + mfv[cur_line].filename + " +" + to_string(mfv[cur_line].line);
+                system(cmd.c_str());
+                endwin();
+                init_screen();
+                refresh_win(win, yWin, xWin, mfv, cur_line);
+                break;
+        }
+
         switch (*keyname(c)) {
             case 'q':
                 do_continue = false;
@@ -70,4 +73,21 @@ int main(int argc, char ** argv)
 
     endwin();
     return 0;
+}
+
+void init_screen()
+{
+    // Ncurses initialization
+    setlocale(LC_ALL,"");
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, true);
+    curs_set(0); // hiden the cursor
+    getmaxyx(stdscr, yMax, xMax);
+    yWin = long(yMax * 0.6);
+    xWin = long(xMax * 0.8);
+    win = newwin(yWin, xWin, (yMax - yWin) /2, (xMax - xWin) / 2);
+    box(win, 0, 0);
+    refresh();
 }
