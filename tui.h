2c9157dcd051a31ef67f9b2413217eb6a6a93550
GenialX <admin@ihuxu.com>
2017-12-02 16:57:54 +0800

    :sparkles: add number

--- tui.h
+++ tui.h
@@ -5,6 +5,10 @@
 
 #include "grep.h"
 
+#ifndef GREPX_TUI_WIN_SHOW_NUM
+#define GREPX_TUI_WIN_SHOW_NUM 1
+#endif
+
 using namespace std;
 
 void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_files> mfv, unsigned long refresh_line);
