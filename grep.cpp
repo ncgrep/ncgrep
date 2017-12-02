f65ce45f2db5dcf8bb5da4bb366f19aab4e0c116
GenialX <admin@ihuxu.com>
2017-12-02 10:57:54 +0800

    :bug: :sparkles: fix bug for read file while more than buffer, change the win size accordding to the current terminal size

--- grep.cpp
+++ grep.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <exception>
 
 #include "grep.h"
 
@@ -9,10 +10,11 @@ using namespace std;
 
 vector<match_files> match_pattern(string file, string parttern) 
 { 
-    char buffer[1024];
+    int line_max_byte = 102400, line = 0;
+    int lastgetp = -2; // the default get position in file is `-1`, in order to distingguish to assign -2
+    char buffer[line_max_byte];
     string tmp;
     ifstream in(file);
-    int line = 0;
     vector<match_files> mfv;
     if (!in.is_open())
     {
@@ -21,7 +23,12 @@ vector<match_files> match_pattern(string file, string parttern)
     while (!in.eof())
     {
         ++line;
-        in.getline(buffer, 1024);
+        in.getline(buffer, line_max_byte);
+        if (lastgetp == in.tellg()) {
+            throw runtime_error("Current line " + to_string(line) + " is more than " + to_string(line_max_byte) + " bytes at file " + file);
+        } else {
+            lastgetp = in.tellg();
+        }
         tmp.assign(buffer);
         if (tmp.find(parttern) != tmp.npos) {
             match_files mf;
