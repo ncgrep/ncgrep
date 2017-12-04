1587db7b2546717299f5b2759290a843bc58f1ea
GenialX <admin@ihuxu.com>
2017-12-05 02:04:11 +0800

    :sparkles: search results support group display, incomplete, currently dizzy, rest

--- tui.h
+++ tui.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "grep.h"
+#include "files.h"
 
 #ifndef NCGREP_TUI_WIN_SHOW_NUM
 #define NCGREP_TUI_WIN_SHOW_NUM 1
@@ -11,4 +12,6 @@
 
 using namespace std;
 
-void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_files> mfv, unsigned long refresh_line);
+void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, vector<match_files> mfv, unsigned long refresh_line);
+void refresh_dirs_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, unsigned long refresh_line);
+void refresh_space_win(WINDOW *win, unsigned long yWin, unsigned long xWin, unsigned long start_line, unsigned long length);
