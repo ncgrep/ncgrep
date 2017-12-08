44306726a0a3c5f6c7d2fd3ed8bfe983ec8f272b
GenialX <admin@ihuxu.com>
2017-12-08 15:30:06 +0800

    :fire: rm build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-g++ -c files.cpp -o files.o
-g++ -c grep.cpp -o grep.o
-g++ -c tui.cpp -o tui.o
-g++ -c data.cpp -o data.o
-g++ -g main.cpp -o ncgrep files.o grep.o tui.o data.o -lncurses
-./ncgrep string . 3
