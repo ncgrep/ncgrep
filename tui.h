6cbdd6d746d6e599ff887e46b322115976b6a8c8
GenialX <admin@ihuxu.com>
2017-12-11 19:13:05 +0800

    :sparkles: support for page-down and page-up ctrl

--- tui.h
+++ tui.h
@@ -10,8 +10,13 @@
 #define NCGREP_TUI_WIN_SHOW_NUM 1
 #endif
 
+#define NCGREP_TUI_DO_MOVE_WIN_LINE true
+#define NCGREP_TUI_DO_NOT_MOVE_WIN_LINE false
+
 using namespace std;
 
-void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, vector<match_files> mfv, unsigned long refresh_line);
-void refresh_dirs_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, unsigned long refresh_line);
+void refresh_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, vector<match_files> mfv, unsigned long refresh_line, bool do_move_win_line);
+void refresh_dirs_win(WINDOW *win, unsigned long yWin, unsigned long xWin, vector<match_dirs> dirs, long cur_dir_index, unsigned long refresh_line, bool do_move_win_line);
 void refresh_space_win(WINDOW *win, unsigned long yWin, unsigned long xWin, unsigned long start_line, unsigned long length);
+unsigned long get_last_win_cur_line();
+void set_last_win_cur_line(unsigned long new_win_cur_line);
